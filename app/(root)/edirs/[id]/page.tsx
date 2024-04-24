721ef23df7af64f74599a022b2bd078ac2ea2727
whiHak <betse1111111111@gmail.com>
2024-04-24 05:08:43 +0300

    fixed conflicts

--- app/(root)/edirs/[id]/page.tsx
+++ app/(root)/edirs/[id]/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AdditionalButtons from "@/components/shared/AdditionalButtons";
 import AddMember from "@/components/shared/AddMember";
 import CheckoutButton from "@/components/shared/CheckoutButton";
@@ -105,28 +104,3 @@ const EdirDetails = async ({
 };
 
 export default EdirDetails;
-=======
-<<<<<<< HEAD
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
-
-export default page
-=======
->>>>>>> b925e6c114ca1b10483a2b9a3b77212fde4f6f17
-import React from "react";
-
-const page = () => {
-  return <div><Posts/></div>;
-};
-
-<<<<<<< HEAD
-export default page;
-=======
-export default Page;
->>>>>>> 32fc3a8835ba83973113190e852e0f7361f926c6
->>>>>>> b925e6c114ca1b10483a2b9a3b77212fde4f6f17
