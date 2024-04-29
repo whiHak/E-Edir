00cc98d780da4779c9793028053c8b642f8eee25
whiHak <betse1111111111@gmail.com>
2024-04-29 08:53:20 +0300

    ...

--- app/api/webhooks/chapa/route.ts
+++ app/api/webhooks/chapa/route.ts
@@ -1,37 +1,31 @@
-// app/api/webhooks/chapa/route.ts
 import { NextApiRequest, NextApiResponse } from "next";
 import crypto from "crypto";
 import { createOrder } from "@/lib/actions/order.actions";
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req: Request, res: Response) {
   // Retrieve your secret key from environment variables
   const SECRET_KEY = process.env.NEXT_PUBLIC_TEST_KEY;
 
-  if (!SECRET_KEY) {
-    return res.status(500).json({
-      error: "SECRET_KEY is not defined in the environment variables.",
-    });
-  }
-
+  
   // Get headers
-  const signature = req.headers["chapa-signature"] as string;
+  const signature = req.headers.get("chapa-signature") as string;
   if (!signature) {
-    return res.status(400).json({ error: "No signature header provided." });
+    // return res.status(400).json({ error: "No signature header provided." });
   }
 
   // Get the body as a string to ensure consistent hashing
-  const body = await req.body;
+  const body = req.body;
   const bodyString = JSON.stringify(body);
 
   // Verify the HMAC signature
   const hash = crypto
-    .createHmac("sha256", SECRET_KEY)
+    .createHmac("sha256", SECRET_KEY!)
     .update(bodyString)
     .digest("hex");
 
   if (signature !== hash) {
     console.error("Invalid signature, possible tampering detected.");
-    return res.status(403).json({ error: "Invalid signature" });
+    // return res.status(403).json({ error: "Invalid signature" });
   }
 
   // Processing the verified webhook data
@@ -47,9 +41,9 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
       createdAt: "2023-02-02T07:53:28.000000Z",
     };
     await createOrder(order);
-    return res.status(200).json({ message: "Webhook received and processed" });
+    // return res.status(200).json({ message: "Webhook received and processed" });
   } catch (error) {
     console.error("Error processing webhook:", error);
-    return res.status(500).json({ error: "Server error" });
+    // return res.status(500).json({ error: "Server error" });
   }
 }
