acccec59ecee03c82691b6f862dc27fe04d78ac4
whiHak <betse1111111111@gmail.com>
2024-04-30 09:20:33 +0300

    ...

--- app/(root)/my-edirs/page.tsx
+++ app/(root)/my-edirs/page.tsx
@@ -5,15 +5,27 @@ import Link from "next/link";
 import Card from "@/components/shared/Card";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
 
-const MyEdirs = () => {
+const MyEdirs = async () => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const user = await getUserById(userId);
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:p-10">
         <h3 className="wrapper h3-bold text-center sm:text-left">My Edir</h3>
-      </section>     
+      </section>
 
-      <Card />
+      {user.edirId ? (
+        <Link href={`/edirs/${user.edirId}`}>
+          <Card />
+        </Link>
+      ) : (
+        <Card />
+      )}
       <ToastContainer />
     </>
   );
